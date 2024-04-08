9f6e25a04caed39074659438b3d44d7ca3939ee5
sren-cuanschutz <siyang.ren@cuanschutz.edu>
2024-04-08 16:48:38 -0600

    do 100 iterations on vbm

--- Dissemination/simulation.py
+++ Dissemination/simulation.py
@@ -3,69 +3,73 @@ import statsmodels.api as sm
 from statsmodels.stats.multitest import multipletests
 import matplotlib.pyplot as plt
 
-na = 1000  # number of group A
-nb = 1000  # number of group B
-s = 256  # number of pixels
+n_a = 1000
+n_b = 1000
+n_image = n_a + n_b 
+n_pixel = 256
+n_iter = 100
+image_size = int(np.sqrt(n_pixel))
+center_size = 8
 
-# set up z
-z = np.concatenate([np.ones(na), np.zeros(nb)])
 
-# set up beta
-beta = np.zeros((16, 16))
-central_start = (16 - 8) // 2  # Calculate the starting index
-central_end = central_start + 8  # Calculate the ending index
+# Simulate data --------------------------------------------------
+# 1. Generate group indicator, 1000 in group A; 1000 in group B 
+# 2. Generate the beta as 16*16 matrix, it has 1 in the center 8*8 and 0 in 
+#    other areas. 
+# 3. Generate the exponential correlation matrix 
+# 4. Use the corr matrix to generate the epsilon as a multivariate normal. 
+#    Epsilon should be (100, 2000, 256)
+# 5. Broadcast and generate y 
+
+group_ind = np.concatenate((np.ones(n_a), np.zeros(n_b)))
+group_ind = group_ind[np.newaxis, :, np.newaxis]
+
+beta = np.zeros((image_size, image_size))
+central_start = (image_size - center_size) // 2
+central_end = central_start + center_size
 beta[central_start:central_end, central_start:central_end] = 1
 beta = beta.flatten()
 
-
-# set up correlation matrix
 def exp_corr_mat(n, rate):
     indices = np.arange(n)
     dist_mat = np.abs(indices - indices[:, np.newaxis])
     corr_mat = np.exp(-rate * dist_mat)
     return corr_mat
 
+corr_mat = exp_corr_mat(n_pixel, rate=1)
 
-corr_mat = exp_corr_mat(s, rate=1)
+epsilon = np.random.multivariate_normal(
+    mean=np.zeros(n_pixel), cov=corr_mat, size=(n_iter, n_image)
+)
+y = beta * group_ind + epsilon
 
-# set up y
-y = np.zeros((na + nb, s))
-for i in range(na + nb):
-    y[i, :] = np.random.multivariate_normal(z[i] * beta, corr_mat)
 
-# VBM
-X = z[:, np.newaxis]  # Makes z a 2D array with a single column
-
-results = []
-for pixel in range(y.shape[1]):
-    # The response variable for the current pixel across all images
-    y_pixel = y[:, pixel]
-
-    model = sm.GLM(y_pixel, X, family=sm.families.Gaussian())
-    res = model.fit()
-    results.append(res)
-
-vbm_pvals = np.array([res.pvalues[0] for res in results])
-vbm_coefs = np.array([res.params[0] for res in results])
-
-# correct p-values for multiple comparison
-vbm_correct_pvals = multipletests(vbm_pvals, method="bonferroni")[1]
-
-# coefficients is a 1D numpy array with the estimated parameters
-# We reshape it to the 2D format of the images (16x16 in your case)
-vbm_coefs_2d = vbm_coefs.reshape((16, 16))
-
-# plot the coefs, black for 1, white for 0
-cmap = plt.cm.gray_r
-plt.imshow(vbm_coefs_2d, cmap=cmap, interpolation="none")
-plt.colorbar(label="Estimated Parameter Value")
-plt.title("2D Visualization of Estimated Parameters")
+# Visualization --------------------------------------------------------
+plt.imshow(y[0, 768, :].reshape((16, 16)), cmap="viridis")
+plt.colorbar(label="Value")
 plt.show()
 
-# plot for corrected p-values, black for < 0.05
-vbm_correct_pvals_2d = vbm_correct_pvals.reshape((16, 16))
-plt.imshow(vbm_correct_pvals_2d, cmap="gray", interpolation="none")
+
+
+# VBM ------------------------------------------------------------------
+p_values = np.zeros((n_iter, n_pixel))
+
+for iter_index in range(n_iter):
+    for pixel_index in range(n_pixel):
+        y_pixel = y[iter_index, :, pixel_index]
+        model = sm.GLM(y_pixel, group_ind[0, :, :], family = sm.families.Gaussian())
+        p_values[iter_index, pixel_index] = model.fit().pvalues[0]
+    p_values[iter_index, :] = multipletests(p_values[iter_index, :], method="bonferroni")[1]
+
+# calculate the perc of p-values < 0.05 for each pixel
+pvals_summ = np.sum(p_values < 0.05, axis=0) / n_iter * 100
+
+plt.imshow(
+    pvals_summ.reshape((image_size, image_size)), 
+    cmap="gray_r", vmin=0, vmax =100, interpolation="none"
+)
 plt.title("Corrected p-values")
+plt.colorbar(label='Percentage of p-values < 0.05')
 plt.xlabel("Pixel X-coordinate")
 plt.ylabel("Pixel Y-coordinate")
 plt.show()
