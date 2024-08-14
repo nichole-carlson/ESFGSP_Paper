9124cb672f19a85617fb31596f73d0ed6c3a01cc
sren-cuanschutz <siyang.ren@cuanschutz.edu>
2024-08-14 15:56:55 -0600

    init model evaluation R script, table 1 for AUC and ACC

---
+++ Simulations/code/03_model_evals.R
@@ -0,0 +1,12 @@
+library(table1)
+
+simulation_dir <- "/Users/siyangren/Documents/ra-cida/ESFGSP_Paper/Simulations"
+code_dir <- file.path(simulation_dir, "code")
+results_data_dir <- file.path(simulation_dir, "results", "data")
+
+load(file = file.path(results_data_dir, "model_metrics_240807.RData"))
+
+table1(~ min_acc + min_auc + `1se_acc` + `1se_auc`, data = sim1_model_perf$pixel)
+table1(~ min_acc + min_auc + `1se_acc` + `1se_auc`, data = sim1_model_perf$freq)
+table1(~ min_acc + min_auc + `1se_acc` + `1se_auc`, data = sim2_model_perf$pixel)
+table1(~ min_acc + min_auc + `1se_acc` + `1se_auc`, data = sim2_model_perf$freq)
