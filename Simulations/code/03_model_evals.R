85980be31fd9c1108bf690259e257cb3819b4701
sren-cuanschutz <siyang.ren@cuanschutz.edu>
2024-08-14 19:22:51 -0600

    minor

--- Simulations/code/03_model_evals.R
+++ Simulations/code/03_model_evals.R
@@ -4,6 +4,8 @@ simulation_dir <- "/Users/siyangren/Documents/ra-cida/ESFGSP_Paper/Simulations"
 code_dir <- file.path(simulation_dir, "code")
 results_data_dir <- file.path(simulation_dir, "results", "data")
 
+
+# ----- Load model fitting results -----
 load(file = file.path(results_data_dir, "model_metrics_240807.RData"))
 
 table1(~ min_acc + min_auc + `1se_acc` + `1se_auc`, data = sim1_model_perf$pixel)
