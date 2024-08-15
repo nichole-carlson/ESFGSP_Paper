8a47b62a6eb6511acce25e9ecec689dcc0a8592a
sren-cuanschutz <siyang.ren@cuanschutz.edu>
2024-08-15 17:23:02 -0600

    adjust stats output for auc and acc

--- Simulations/code/03_model_evals.R
+++ Simulations/code/03_model_evals.R
@@ -8,7 +8,28 @@ results_data_dir <- file.path(simulation_dir, "results", "data")
 # ----- Load model fitting results -----
 load(file = file.path(results_data_dir, "model_metrics_240807.RData"))
 
-table1(~ min_acc + min_auc + `1se_acc` + `1se_auc`, data = sim1_model_perf$pixel)
-table1(~ min_acc + min_auc + `1se_acc` + `1se_auc`, data = sim1_model_perf$freq)
-table1(~ min_acc + min_auc + `1se_acc` + `1se_auc`, data = sim2_model_perf$pixel)
-table1(~ min_acc + min_auc + `1se_acc` + `1se_auc`, data = sim2_model_perf$freq)
+my.render.cont <- function(x) {
+  with(stats.default(x), sprintf("%.3f (%0.3f)", MEAN, SD))
+}
+
+
+table1(
+  ~ min_acc + min_auc + `1se_acc` + `1se_auc`,
+  data = sim1_model_perf$pixel,
+  render.continuous = my.render.cont
+)
+table1(
+  ~ min_acc + min_auc + `1se_acc` + `1se_auc`,
+  data = sim1_model_perf$freq,
+  render.continuous = my.render.cont
+)
+table1(
+  ~ min_acc + min_auc + `1se_acc` + `1se_auc`,
+  data = sim2_model_perf$pixel,
+  render.continuous = my.render.cont
+)
+table1(
+  ~ min_acc + min_auc + `1se_acc` + `1se_auc`,
+  data = sim2_model_perf$freq,
+  render.continuous = my.render.cont
+)
