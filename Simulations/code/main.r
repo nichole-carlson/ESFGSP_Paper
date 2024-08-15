0a1e1cee7e080896c2e8572b8f4151021f338687
sren-cuanschutz <siyang.ren@cuanschutz.edu>
2024-08-15 17:23:17 -0600

    move code out

--- Simulations/code/main.r
+++ Simulations/code/main.r
@@ -1,51 +1,3 @@
-# Function for Simulation 1
-simulate_1 <- function(i, size, n_samples, effect, p_train, n_perm, seed) {
-  set.seed(seed + i)
-  w <- generate_cov_matrix(size)
-  beta <- define_beta(size, effect)
-  x <- generate_X(n_samples, size, w)
-  y <- generate_response(x, beta)
-  perform_metrics <- perform_lasso(x, y, p_train, seed = seed + i)
-  p_vals <- perm_lasso(x, y, n_perm, seed = seed + i)
-  cbind(perform_metrics, p_vals)
-}
-
-
-# Run Simulation 1
-# For each iteration:
-#   - Split into train and test dataset, calculate AUC and accuracy, estimated
-#     coefficients under lambda.min and lambda.1se
-#   - Perform permutation test. Estimate p-values for pixels
-
-tic()
-sim1_output <- simWrapper(
-  n_sim = 500,
-  f_sim = function(i) simulate_1(i, size = 16, n_samples = 1000, effect = 0.1, p_train = 0.8, n_perm = 100, seed = 42),
-  list_export = c(
-    "generate_cov_matrix", "define_beta", "generate_X", "generate_probs",
-    "generate_response", "simulate_1", "perform_lasso", "perm_lasso"
-  ),
-  list_package = c("MASS", "glmnet", "pROC", "foreach", "doParallel")
-)
-toc()
-
-
-
-
-
-
-# # Save Simulation 1 results
-# save(
-#   sim1_output,
-#   file = file.path(parent_dir, "Simulations", paste0("sim1_", format(Sys.time(), "%y%m%d"), ".RData"))
-# )
-load(file = file.path(current_dir, "Simulations", "sim1_240701.RData"))
-
-# # Generate summary stats for ACC and AUCs
-acc_auc_sim1 <- as.data.frame(sim1_output[, 1:4])
-colnames(acc_auc_sim1) <- c("min_acc", "min_auc", "1se_acc", "1se_auc")
-table1(~ min_acc + min_auc + `1se_acc` + `1se_auc`, data = acc_auc_sim1)
-
 # # Calculate significance percentages for Simulation 1
 pvals_matrix_sim1 <- sim1_output[, -(1:4)]
 sig_perc_sim1 <- calc_sig_perc(pvals_matrix_sim1, method = "bonferroni", alpha = 0.05)
@@ -66,45 +18,6 @@ ggsave(
   plot = sim1_heatmap, width = 10, height = 8
 )
 
-# Function for Simulation 2
-simulate_2 <- function(i, size, n_samples, sparsity, effect, p_train, n_perm, seed) {
-  set.seed(seed + i)
-  w <- generate_cov_matrix(size)
-  v <- eigen_decomp(w)$vectors
-  x <- generate_X(n_samples, size, diag(size^2))
-  b <- generate_sparse_vector(size^2, sparsity, effect, seed)
-  y <- generate_response(x, b)
-  perform_metrics <- perform_lasso(x, y, p_train, seed = seed + i)
-  p_vals <- perm_lasso(x, y, n_perm, seed = seed + i)
-  cbind(perform_metrics, p_vals)
-}
-
-
-
-# Run Simulation 2
-tic()
-sim2_output <- simWrapper(
-  n_sim = 500,
-  f_sim = function(i) simulate_2(i, size = 16, n_samples = 1000, sparsity = 0.1, effect = 0.4, p_train = 0.8, n_perm = 100, seed = 42),
-  list_export = c(
-    "generate_cov_matrix", "eigen_decomp", "generate_sparse_vector", "generate_X", "generate_probs", "generate_response", "perform_lasso",
-    "perm_lasso", "simulate_2"
-  ),
-  list_package = c("MASS", "glmnet", "pROC", "foreach", "doParallel")
-)
-toc()
-
-# Save Simulation 2 results
-# save(
-#   sim2_output,
-#   file = file.path(parent_dir, "Simulations", paste0("sim2_", format(Sys.time(), "%y%m%d"), ".RData"))
-# )
-load(file = file.path(current_dir, "Simulations", "sim2_240701.RData"))
-
-# Generate summary stats for ACC and AUCs for Simulation 2
-acc_auc_sim2 <- as.data.frame(sim2_output[, 1:4])
-colnames(acc_auc_sim2) <- c("min_acc", "min_auc", "1se_acc", "1se_auc")
-table1(~ min_acc + min_auc + `1se_acc` + `1se_auc`, data = acc_auc_sim2)
 
 # Calculate significance percentages for Simulation 2
 pvals_matrix_sim2 <- sim2_output[, -(1:4)]
