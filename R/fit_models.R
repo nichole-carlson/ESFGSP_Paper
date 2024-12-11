319f4db6e3bb1f074e3a1c08d1c91156aede2873
sren <sren@cuanschutz.edu>
2024-12-11 10:12:23 -0700

    a new scripts with functions for training models.

---
+++ R/fit_models.R
@@ -0,0 +1,45 @@
+library(glmnet)
+library(pROC)
+library(tictoc)
+library(hdi)
+
+
+# Splits the dataset into training and testing sets.
+#
+# Args:
+#  x: A data frame or matrix with observations as rows and features as columns.
+#  y: A vector of target values with the same number of observations as 'x'.
+#  p_train: A float value (0-1) specifying the proportion of data for training.
+#  seed: An optional integer for reproducibility.
+#
+# Returns:
+#  train: A list with 'x' and 'y'.
+#  test: A list with 'x' and 'y'.
+
+train_test_split <- function(x, y, p_train, seed = NULL) {
+  if (!is.null(seed)) {
+    set.seed(seed)
+  }
+
+  # Check whether x and y have the same # of obs
+  if (nrow(x) != length(y)) {
+    stop("The number of rows in 'x' does not match the length of 'y'.")
+  }
+
+  # Calculate the train data size
+  n <- nrow(x)
+  train_idx <- sample(seq_len(n), size = floor(n * p_train))
+
+  # Split x and y into train and test
+  x_train <- x[train_idx, , drop = FALSE]
+  x_test <- x[-train_idx, , drop = FALSE]
+  y_train <- y[train_idx]
+  y_test <- y[-train_idx]
+
+  results <- list(
+    train = list(x = x_train, y = y_train),
+    test = list(x = x_test, y = y_test)
+  )
+
+  return(results)
+}
