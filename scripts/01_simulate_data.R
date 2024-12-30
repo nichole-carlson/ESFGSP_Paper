dd3f88d4dad213b54e72c064de7d88b7d1fb2348
sren <sren@cuanschutz.edu>
2024-12-30 10:08:35 +0800

    print info.

--- scripts/01_simulate_data.R
+++ scripts/01_simulate_data.R
@@ -16,9 +16,11 @@ beta_effect <- 0.1
 b_effect <- 0.2
 
 
-# Simulation 1a. Exp decay adjacency matrix, on pixel space.
+# Simulation 1a. Exp decay adjacency matrix, in pixel space.
 sim1a_exp_pixel_data <- list()
 
+cat("Running simulation 1 in pixel space ...\n")
+tic()
 for (i in seq_len(n_iter)) {
   sim1a_exp_pixel_data[[i]] <- run_pixel_to_freq_simulation(
     n_samples = n_samples,
@@ -29,7 +31,11 @@ for (i in seq_len(n_iter)) {
     seed = seed
   )
 }
+cat("Simulation completed. \n")
+toc()
 
+cat("Saving simulated data as a .RData file ...\n")
+tic()
 save(
   sim1a_exp_pixel_data,
   file = file.path(
@@ -37,41 +43,42 @@ save(
     paste0("sim1a_pixel_data_", format(Sys.Date(), "%y%m%d"), ".RData")
   )
 )
+toc()
 
 
-# Simulation 1b. Exp decay adjacency matrix, on freq space.
-sim1b_exp_freq_data <- list()
-
-for (i in seq_len(n_iter)) {
-  sim1b_exp_freq_data[[i]] <- run_simulation_1b(
-    n_samples = n_samples,
-    n_row = n_row,
-    n_col = n_col,
-    sparse_level = sparse_level,
-    effect_size = b_effect,
-    seed = seed
-  )
-}
-
-save(
-  sim1b_exp_freq_data,
-  file = file.path(
-    proj_dir, "results",
-    paste0("sim1b_freq_data_", format(Sys.Date(), "%y%m%d"), ".RData")
-  )
-)
-
-
-# Simulation 2a. 2-neighbor adjacency matrix, on pixel space.
-sim2a_2n_pixel_data <- list()
-
-for (i in seq_len(n_iter)) {
-  sim2a_2n_pixel_data[[i]] <- run_pixel_to_freq_simulation(
-    n_samples = n_samples,
-    n_row = n_row,
-    n_col = n_col,
-    effect_size = beta_effect,
-    c_adj = generate_n_neighbor_matrix(n_row, n_col, d_max = 2),
-    seed = seed
-  )
-}
+# # Simulation 1b. Exp decay adjacency matrix, on freq space.
+# sim1b_exp_freq_data <- list()
+#
+# for (i in seq_len(n_iter)) {
+#   sim1b_exp_freq_data[[i]] <- run_simulation_1b(
+#     n_samples = n_samples,
+#     n_row = n_row,
+#     n_col = n_col,
+#     sparse_level = sparse_level,
+#     effect_size = b_effect,
+#     seed = seed
+#   )
+# }
+#
+# save(
+#   sim1b_exp_freq_data,
+#   file = file.path(
+#     proj_dir, "results",
+#     paste0("sim1b_freq_data_", format(Sys.Date(), "%y%m%d"), ".RData")
+#   )
+# )
+#
+#
+# # Simulation 2a. 2-neighbor adjacency matrix, on pixel space.
+# sim2a_2n_pixel_data <- list()
+#
+# for (i in seq_len(n_iter)) {
+#   sim2a_2n_pixel_data[[i]] <- run_pixel_to_freq_simulation(
+#     n_samples = n_samples,
+#     n_row = n_row,
+#     n_col = n_col,
+#     effect_size = beta_effect,
+#     c_adj = generate_n_neighbor_matrix(n_row, n_col, d_max = 2),
+#     seed = seed
+#   )
+# }
