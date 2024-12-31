fd6862833f63968a10461484fc1208d4c0f21e00
Siyang Ren <siyang.ren@cuanschutz.edu>
2024-12-31 05:23:38 -0700

    increase cpu cores.

--- jobs/run_model_fitting.sh
+++ jobs/run_model_fitting.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name="model"
 #SBATCH --output=logs/model1a_%j.out
 #SBATCH --ntasks=1                       # Number of tasks (1 node)
-#SBATCH --cpus-per-task=8                # Number of CPU cores per task
+#SBATCH --cpus-per-task=48                # Number of CPU cores per task
 #SBATCH --mem=120G                       # Memory per node
 #SBATCH --partition=amilan               # Partition for array tasks
 #SBATCH --time=24:00:00                  # Max runtime
