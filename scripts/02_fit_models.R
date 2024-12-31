4ad11e34e6977ec0717f7a3a9ded8c0a2a93a3bf
sren <sren@cuanschutz.edu>
2024-12-31 12:25:32 +0800

    convert to parallel.

--- scripts/02_fit_models.R
+++ scripts/02_fit_models.R
@@ -1,3 +1,4 @@
+# Determine project and data dirs based on the operating system
 sys_info <- Sys.info()
 if (!is.null(sys_info) && sys_info["sysname"] == "Linux") {
   proj_dir <- "/projects/sren@xsede.org/ESFGSP_Paper"
@@ -10,10 +11,51 @@ if (!is.null(sys_info) && sys_info["sysname"] == "Linux") {
 # Source functions for fitting LASSO models
 source(file.path(proj_dir, "R", "fit_models.R"))
 source(file.path(proj_dir, "R", "simulate_data.R"))
+source(file.path(proj_dir, "R", "sim_wrapper.R"))
 
 p_train <- 0.8
 seed <- 42
+packages <- c("glmnet", "pROC", "tictoc", "hdi", "MASS")
 
+
+# Define the task function for parallel processing
+fit_lasso_task <- function(dat, p_train, lambda, seed) {
+  # Fit model on data in the pixel space
+  pixel_model_metrics <- run_lasso_pipeline(
+    x = dat$x,
+    y = dat$y,
+    p = p_train,
+    lambda = lambda,
+    seed = seed
+  )
+
+  # Transform estimated coefs to the freq space
+  eigen_vectors <- dat$hparams$eigen_vectors
+  pixel_model_metrics$coefs_freq <- t(eigen_vectors) %*% pixel_model_metrics$coefs
+
+  # Fit model on data in the freq space
+  freq_model_metrics <- run_lasso_pipeline(
+    x = dat$x_freq,
+    y = dat$y,
+    p = p_train,
+    lambda = lambda,
+    seed = seed
+  )
+
+  # Transform estimated coefs to the pixel space
+  freq_model_metrics$coefs_pixel <- eigen_vectors %*% freq_model_metrics$coefs
+
+  # Return results
+  results <- list(
+    pixel = pixel_model_metrics,
+    freq = freq_model_metrics
+  )
+
+  return(results)
+}
+
+
+# ---------- Simulation 1 ----------
 # Load simulation 1a list of data and fit LASSO models
 cat("Loading Simulation 1a .RData file ...\n")
 tic()
@@ -22,39 +64,17 @@ toc()
 
 cat("Fitting models on Simulation 1a data ... \n")
 tic()
-sim1a_lasso_results <- list()
-for (i in seq_along(sim1a_exp_pixel_data)) {
-  dat <- sim1a_exp_pixel_data[[i]]
-
-  # Fit model on data in the pixel space, get AUC, ACC and estimated coefs
-  pixel_model_metrics <- run_lasso_pipeline(
-    x = dat$x,
-    y = dat$y,
-    p = p_train,
-    lambda = "lambda.min",
-    seed = seed
-  )
-
-  # Transform estimated coefs to the freq space
-  eigen_vectors <- dat$hparams$eigen_vectors
-  pixel_model_metrics$coefs_freq <- t(eigen_vectors) %*% pixel_model_metrics$coefs
-
-  sim1a_lasso_results[["pixel"]][[i]] <- pixel_model_metrics
-
-  # Fit model on data in the freq space, get AUC, ACC and estimated coefs
-  freq_model_metrics <- run_lasso_pipeline(
-    x = dat$x_freq,
-    y = dat$y,
-    p = p_train,
-    lambda = "lambda.min",
-    seed = seed
-  )
-
-  # Transform estimated coefs to the pixel space
-  freq_model_metrics$coefs_pixel <- eigen_vectors %*% freq_model_metrics$coefs
-
-  sim1a_lasso_results[["freq"]][[i]] <- freq_model_metrics
-}
+# Create a list of arguments for parallel execution
+arg_list <- lapply(sim1a_exp_pixel_data, function(dat) {
+  list(dat = dat, p_train = p_train, lambda = "lambda.min", seed = seed)
+})
+# Use parallel_wrapper to fit models in parallel
+sim1a_lasso_results <- parallel_wrapper(
+  task_fn = fit_lasso_task,
+  args_list = arg_list,
+  cores = 12,
+  pkgs = packages
+)
 toc()
 
 cat("Saving fitted model as a .RData file ...\n")
