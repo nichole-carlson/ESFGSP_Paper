330464e302fd71d168242b0da57fec620eeff94e
Siyang Ren <siyang.ren@cuanschutz.edu>
2025-05-21 21:26:40 -0400

    add sim_id, make it a Rscript ready to run.

--- scripts/simulation_1.R
+++ scripts/simulation_1.R
@@ -15,11 +15,109 @@
 #      to the frequency space.
 #   2. Transform X into frequency space, fit y using X_freq, and convert the
 #      estimated coefficients back to pixel space.
-#
-# The simulation is repeated 500 times.
+# Each model will be fitted on either lambda.min or lambda.1se
+
+library(optparse)
 
 proj_dir <- "/Users/siyangren/Documents/projects/ESFGSP_Paper"
 
 source(file.path(proj_dir, "R", "simulate_data.R"))
 source(file.path(proj_dir, "R", "utils.R"))
 source(file.path(proj_dir, "R", "fit_model.R"))
+
+
+# ---------- Capture parameters ----------
+option_list <- list(
+  optparse::make_option(
+    "--sim_id",
+    type = "integer",
+    help = "Simulation index"
+  ),
+  optparse::make_option(
+    "--effect",
+    type = "double",
+    default = 0.1,
+    help = "Effect size [default %default]"
+  ),
+  optparse::make_option(
+    "--n_sample",
+    type = "integer",
+    default = 1000,
+    help = "Number of samples per iteration [default %default]"
+  ),
+  optparse::make_option(
+    "--out_dir",
+    type = "character",
+    help = "Directory to save the output file"
+  ),
+  optparse::make_option(
+    "--seed",
+    type = "character",
+    default = NULL,
+    help = "Random seed (optional)"
+  )
+)
+
+opt_parser <- optparse::OptionParser(option_list = option_list)
+opt <- optparse::parse_args(opt_parser)
+
+# Convert seed if provided
+seed <- if (!is.null(opt$seed)) as.numeric(opt$seed) else NULL
+
+if (!is.null(seed)) set.seed(seed)
+
+
+# ---------- Simulate Data ----------
+# Exponential decay correlation matrix
+cov_matrix <- gen_exp_corr_mat(n_row = 16, n_col = 16)
+
+# Simulate X ~ MVN(0, cov_matrix)
+x <- simulate_mvn_samples(n_sample = opt$n_sample, cov_matrix = cov_matrix)
+
+# Generate 2D coeffcient matrix and flatten to 1D
+beta_matrix <- matrix(0, nrow = 16, ncol = 16)
+beta_matrix[5:12, 5:12] <- opt$effect
+beta_vec <- as.vector(beta_matrix)
+
+# Simulate y
+y <- simulate_pixel_outcome(x = x, beta = beta_vec, from_pixel_space = TRUE)
+
+
+
+# ---------- Calculate Eigenvector for Transformation ----------
+# Derive adjacency matrix from correlation matrix
+adj_matrix <- cov_matrix - diag(nrow(cov_matrix)) # adj matrix has diag = 0
+# Eigendecomposition on MCM, C = adj matrix
+eigen_mat_data <- eigen_decomp_mcm(adj_matrix)$vectors
+
+
+
+# ---------- Fit LASSO Model in Pixel and Freq Spaces ----------
+spaces <- c("pixel", "freq")
+lambdas <- c("lambda.min", "lambda.1se")
+
+lasso_results <- list()
+
+for (space in spaces) {
+  lasso_results[[space]] <- list()
+  for (lam in lambdas) {
+    if (space == "pixel") {
+      # models fitted on pixel space
+      lasso_results[[space]][[lam]] <- lasso_pixel_or_freq(
+        x, y,
+        lambda = lam
+      )
+    } else {
+      # models fitted on freq space
+      lasso_results[[space]][[lam]] <- lasso_pixel_or_freq(
+        x, y,
+        in_pixel_space = FALSE, e = eigen_mat_data, lambda = lam
+      )
+    }
+  }
+}
+
+
+# Save as a rds file
+filename <- paste0("sim_", sprintf("%03d", opt$sim_id), ".rds")
+saveRDS(lasso_results, file = file.path(opt$out_dir, filename))
