49cd7c8dd1fc4ea6abb024232134f9c5bb60d5f5
Siyang Ren <siyang.ren@cuanschutz.edu>
2025-06-01 09:36:37 -0400

    init

--- scripts/visualize_sim1_results.R
+++ scripts/visualize_sim1_results.R
@@ -17,3 +17,26 @@
 #   - A scatterplot of the corresponding coefficients in freq space
 #   - A heatmap of p<0.05 in pixel space
 #   - A scatterplot of the corresponding p<0.05 in freq space
+
+library(rprojroot)
+library(ggplot2)
+
+proj_dir <- rprojroot::find_root(rprojroot::is_git_root)
+p_drive <- "/Volumes/alzheimersdisease/ESFGSPproject/DataLibrary"
+
+source(file.path(proj_dir, "R", "summarize_results.R"))
+
+# Read the .rds file saving all iterations
+res <- readRDS(file.path(p_drive, "sim1_combined_results.rds"))
+
+# Matrix of eigenvectors (decreasingly ordered)
+e <- res$data[[1]]$e
+
+# Simulation 1 true coefs in pixel space
+beta_matrix <- matrix(0, nrow = 16, ncol = 16)
+beta_matrix[5:12, 5:12] <- 0.1
+beta_vec <- as.vector(beta_matrix)
+
+# true coefs transformed to freq space: coef_freq = t(e) %*% coef_pixel
+b_vec <- t(e) %*% matrix(beta_vec, ncol = 1) |>
+  as.vector()
