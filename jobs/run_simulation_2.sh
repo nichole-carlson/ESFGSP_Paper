25a40832825e519db44433f3c3e70fbeabba5e19
Siyang Ren <siyang.ren@cuanschutz.edu>
2025-06-11 12:59:12 -0600

    fix typo

--- jobs/run_simulation_2.sh
+++ jobs/run_simulation_2.sh
@@ -8,7 +8,7 @@
 #SBATCH --array=1-500
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=siyang.ren@cuanschutz.edu
-#SBATCH --output=logs/sim1_%A.log
+#SBATCH --output=logs/sim2_%A.log
 
 module load R
 
@@ -17,11 +17,10 @@ OUT_DIR=/scratch/alpine/sren@xsede.org/esfgsp/sim2_iters
 SIM_ID=${SLURM_ARRAY_TASK_ID}
 SEED=$((10000 + SIM_ID))
 
-Rscript --no-save scripts/simulation_1.R \
+Rscript --no-save scripts/simulation_2.R \
         --sim_id $SIM_ID \
         --out_dir "$OUT_DIR" \
         --effect 0.03 \
         --sparsity 0.1 \
         --n_sample 1000 \
         --seed $SEED
-
