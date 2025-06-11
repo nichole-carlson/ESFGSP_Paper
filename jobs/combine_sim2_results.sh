099d34b4ba443e45299b505071fcae55d4f39e4b
Siyang Ren <siyang.ren@cuanschutz.edu>
2025-06-11 12:59:47 -0600

    prepare combine sim2 results

--- jobs/combine_sim2_results.sh
+++ jobs/combine_sim2_results.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-#SBATCH --job-name=s1_com
+#SBATCH --job-name=s2_com
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=6G
-#SBATCH --partition=amilan
+#SBATCH --partition=amiland
 #SBATCH --time=00:30:00
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=siyang.ren@cuanschutz.edu
-#SBATCH --output=logs/sim1_combine_%j.log
+#SBATCH --output=logs/sim2_combine_%j.log
 
 module load R
 
 DATA_DRIVE="/scratch/alpine/sren@xsede.org/esfgsp"
 
-RESULT_DIR="${DATA_DRIVE}/sim1_iters"
+RESULT_DIR="${DATA_DRIVE}/sim2_iters"
 OUT_DIR="${DATA_DRIVE}"
 
 Rscript --no-save scripts/combine_sim_results.R \
         --indir $RESULT_DIR \
         --outdir $OUT_DIR \
-        --sim_id "sim1"
+        --sim_id "sim2"
