3b6c79aa8b902a7c7fb11efa9d9ed1093106d51d
Siyang Ren <siyang.ren@cuanschutz.edu>
2025-06-11 20:22:02 -0400

    add p-value visualization; functionalize previous visualizations

--- scripts/visualize_sim1_results.R
+++ scripts/visualize_sim1_results.R
@@ -25,9 +25,9 @@ library(ggplot2)
 library(patchwork)
 
 proj_dir <- rprojroot::find_root(rprojroot::is_git_root)
-data_dir <- "/Volumes/alzheimersdisease/ESFGSPproject/DataLibrary"
+data_dir <- "/Volumes/alzheimersdisease/ESFGSPproject/DataLibrary/sim1"
 
-# vector_to_heatmap, plot_scatter
+# vector_to_heatmap, plot_scatter, create_heatmap_plots, create_scatter_plots
 source(file.path(proj_dir, "R", "summarize_results.R"))
 # transform_data, transform_coef
 source(file.path(proj_dir, "R", "transformations.R"))
@@ -46,6 +46,11 @@ auc_acc_df <- res$auc_acc
 coef_arr <- res$coefs
 p_arr <- res$pvals
 
+p <- length(p)
+index <- seq_len(p) / p
+x_freq <- transform_data(x, e, to_freq = TRUE)
+b <- transform_coef(beta, e, to_freq = TRUE)
+
 
 # ---------- Group Mean Difference ----------
 # In the pixel space
@@ -53,8 +58,6 @@ p_group_mean_pixel <- vector_to_heatmap(
   colMeans(x[y == 1, ]) - colMeans(x[y == 0, ])
 )
 # In the freq space
-index <- seq_along(beta) / length(beta)
-x_freq <- transform_data(x, e, to_freq = TRUE)
 p_group_mean_freq <- plot_scatter(
   x = index,
   y = colMeans(x_freq[y == 1, ]) - colMeans(x_freq[y == 0, ])
@@ -62,67 +65,38 @@ p_group_mean_freq <- plot_scatter(
 
 
 # ---------- Ture Coefficients Visualization ----------
-b <- transform_coef(beta, e, to_freq = TRUE)
-
 # Heatmap for true beta
 p_true_beta <- vector_to_heatmap(beta)
 
 # Scatter plot of b with x-axis as scaled index: i/256 for i = 1 to 256
-index <- seq_along(b) / length(b)
-p_true_b <- plot_scatter(x = x_index, y = b)
+p_true_b <- plot_scatter(x = index, y = b)
 
 
 # ---------- Average Estimated Coefs ----------
-# Average estimated beta, fit on pixel space
-beta_pixel <- coef_arr[, , "pixel", ]
-beta_pixel_avg <- apply(beta_pixel, MARGIN = c(1, 2), FUN = mean)
-p_est_beta_pixel <- {
-  lim <- range(beta_pixel_avg) # lower and upper
+# Average est coefs fit in pixel space
+beta_pixel_avg <- apply(coef_arr[, , "pixel", ], MARGIN = c(1, 2), FUN = mean)
+# Average est coefs fit in freq space
+b_freq_avg <- apply(coef_arr[, , "freq", ], MARGIN = c(1, 2), FUN = mean)
 
-  plot_list <- lapply(colnames(beta_pixel_avg), function(l) {
-    .vec <- beta_pixel_avg[, l]
-    vector_to_heatmap(.vec, lim) +
-      labs(subtitle = l) +
-      theme(plot.subtitle = element_text(hjust = 0.5))
-  })
-  patchwork::wrap_plots(plot_list, ncol = 2)
-}
+p_est_beta_pixel <- create_heatmap_plots(beta_pixel_avg)
+p_est_b_pixel <- create_scatter_plots(transform_coef(beta_pixel_avg, e), index)
+p_est_b_freq <- create_scatter_plots(b_freq_avg, index)
+p_est_beta_freq <- create_heatmap_plots(transform_coef(b_freq_avg, e))
 
-# Average estimated b, fit in pixel space
-b_pixel_avg <- transform_coef(beta_pixel_avg, e)
-p_est_b_pixel <- {
-  plot_list <- lapply(colnames(b_pixel_avg), function(l) {
-    .vec <- b_pixel_avg[, l]
-    plot_scatter(x_index, .vec) +
-      labs(subtitle = l) +
-      theme(plot.subtitle = element_text(hjust = 0.5))
-  })
-  patchwork::wrap_plots(plot_list, ncol = 2)
-}
 
-# Average estimated b, fit in freq space
-b_freq <- coef_arr[, , "freq", ]
-b_freq_avg <- apply(b_freq, MARGIN = c(1, 2), FUN = mean)
-p_est_b_freq <- {
-  plot_list <- lapply(colnames(b_freq_avg), function(l) {
-    .vec <- b_freq_avg[, l]
-    plot_scatter(x_index, .vec) +
-      labs(subtitle = l) +
-      theme(plot.subtitle = element_text(hjust = 0.5))
-  })
-  patchwork::wrap_plots(plot_list, ncol = 2)
-}
 
-# Average estimated beta, fit in freq space
-beta_freq_avg <- transform_coef(b_freq_avg, e)
-p_est_beta_freq <- {
-  lim <- range(beta_freq_avg)
+# ---------- Percentage of p<0.05 Visualization ----------
+# Percentage of p<0.05 for the model fit on pixel space
+pixel_pval <- apply(
+  p_arr[, , "pixel", ],
+  MARGIN = c(1, 2),
+  FUN = function(x) mean(x < 0.05)
+)
+freq_pval <- apply(
+  p_arr[, , "freq", ],
+  MARGIN = c(1, 2),
+  FUN = function(x) mean(x < 0.05)
+)
 
-  plot_list <- lapply(colnames(beta_freq_avg), function(l) {
-    .vec <- beta_freq_avg[, l]
-    vector_to_heatmap(.vec, lim) +
-      labs(subtitle = l) +
-      theme(plot.subtitle = element_text(hjust = 0.5))
-  })
-  patchwork::wrap_plots(plot_list, ncol = 2)
-}
+p_pixel_pval <- create_heatmap_plots(pixel_pval)
+p_freq_pval <- create_scatter_plots(freq_pval, index)
